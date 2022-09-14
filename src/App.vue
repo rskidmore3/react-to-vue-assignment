4b440e819e89517b3d7a1535df37fa0619f1308f
ethika-rskidmore <ethika-rskidmore@ethika-rskidmores-MacBook-Pro.local>
2022-09-14 15:54:24 -0700

    Div for list made.

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,39 @@
 </script>
 
 <template>
-  <p class="red-text">Salutations terrafirma</p>
+
+<div class="border red-background div-height div-width" >
+  <div class="header-border header-size">
+    
+  </div>
+
+</div>
+
 </template>
 
 <style scoped>
+.header-border{
+  border-bottom: 1px  solid black; 
+}
+
+.header-size{
+  height: 50px;
+  width: 100%; 
+}
 .red-text {
   color: red;
 }
+
+.div-height{
+  height: 500px;
+}
+.div-width{
+  width: 300px;
+}
+
+.border{
+  border: 1px solid black;
+  border-radius: 5px;
+}
+
 </style>

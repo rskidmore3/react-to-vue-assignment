7d391b109c77ad806ba6b5423ac8d6750d10bc95
rskidmore3 <rskidmore@ethika.com>
2022-09-15 10:34:10 -0700

    Imported Navbar to App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
+  <Navbar />
 
 <div class="border red-background rounded" style="width: 300px; height: 500px; ">
   <div class="header-border header-size border-bottom border-black d-flex justify-content-center" style="width: 100%; height: 50px">    

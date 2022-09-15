41cbb8c07f294bbf7992b61de66179e37b5c75fb
rskidmore3 <rskidmore@ethika.com>
2022-09-15 11:47:34 -0700

    Input made for Name, Publisher, and Rating.

--- src/components/AddGame.vue
+++ src/components/AddGame.vue
@@ -2,18 +2,22 @@
 <template>
 <form>
     <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="nameOfGame" aria-describedby="nameOfGame">
     </div>
+    
     <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <label for="publisher" class="form-label">Publisher</label>
+        <input type="text" class="form-control" id="publisher">
     </div>
-    <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Rating</label>
+        <input type="number" class="form-control" id="rating">
     </div>
+
+
+    
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </template>

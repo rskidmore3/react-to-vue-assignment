fa59fc0406f9da05973852a4dab7f766d047296a
rskidmore3 <rskidmore@ethika.com>
2022-09-15 11:18:05 -0700

    Abstracted Navbar html to Navbar component.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   // import Navbar from './components/Navbar.vue'
   import GamesList from './components/GamesList.vue'
   import AddGame from './components/AddGame.vue'
-
+import Navbar from './components/Navbar.vue'
+ 
   const routes ={
     '/': GamesList, 
     '/addgame': AddGame
@@ -10,51 +11,31 @@
 
 
   export default {
-  data() {
-    return {
-      currentPath: window.location.hash
-    }
-  },
-  computed: {
-    currentView() {
-      return routes[this.currentPath.slice(1) || '/'] || NotFound
-    }
-  },
-  mounted() {
-    window.addEventListener('hashchange', () => {
-		  this.currentPath = window.location.hash
-		})
-  }
+    data() {
+        return {
+            currentPath: window.location.hash
+        };
+    },
+    computed: {
+        currentView() {
+            return routes[this.currentPath.slice(1) || "/"] || NotFound;
+        }
+    },
+    mounted() {
+        window.addEventListener("hashchange", () => {
+            this.currentPath = window.location.hash;
+        });
+    },
+    components: { Navbar }
 }
  
-
 </script>
 
 <template>
-  <!-- <Navbar />
-  <GamesList/> -->
 
-  <nav class="navbar navbar-expand-lg bg-light mb-1">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#/">Games List</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#/addgame">Add Game</a>
-            </li>
-            
-        </ul>
-        </div>
-    </div>
-    </nav>
-
-    <component :is="currentView" />
+  <Navbar/>
+ 
+  <component :is="currentView" />
 
 
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,25 @@
 
 
 <template>
-hello
+  <nav class="navbar navbar-expand-lg bg-light mb-1">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#/">Games List</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#/addgame">Add Game</a>
+            </li>
+            
+        </ul>
+        </div>
+    </div>
+    </nav>
 </template>
 
 

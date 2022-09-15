c1d7ea1685c8c035831f31143385993eea78632c
rskidmore3 <rskidmore@ethika.com>
2022-09-15 11:04:17 -0700

    Basic routing added. Add AddGame.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
-<script setup>
-import Navbar from './components/Navbar.vue'
+<script  >
+  // import Navbar from './components/Navbar.vue'
+  import GamesList from './components/GamesList.vue'
+  import AddGame from './components/AddGame.vue'
+
+  const routes ={
+    '/': GamesList, 
+    '/addgame': AddGame
+  }
+
+
+  export default {
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/'] || NotFound
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+		  this.currentPath = window.location.hash
+		})
+  }
+}
+ 
+
 </script>
 
 <template>
-  <Navbar />
+  <!-- <Navbar />
+  <GamesList/> -->
 
-<div class="border red-background rounded" style="width: 300px; height: 500px; ">
-  <div class="header-border header-size border-bottom border-black d-flex justify-content-center" style="width: 100%; height: 50px">    
-    <h2> 
-      Games List
-    </h2>
-  </div>
-  <div v-for="(item, index) in $store.state.games" :key="index">{{ item.name }} {{ item.publisher }} {{ item.rating }}</div>
-</div>
+  <nav class="navbar navbar-expand-lg bg-light mb-1">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#/">Games List</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#/addgame">Add Game</a>
+            </li>
+            
+        </ul>
+        </div>
+    </div>
+    </nav>
+
+    <component :is="currentView" />
+
 
 </template>
---
+++ src/components/AddGame.vue
@@ -0,0 +1,4 @@
+<script></script>
+<template>
+    Add game: coming soon. 
+</template>

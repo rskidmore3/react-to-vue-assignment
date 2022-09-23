ce39c3e7f20f5c7341feba89240f49302112043a
rskidmore3 <rskidmore@ethika.com>
2022-09-23 10:31:33 -0700

    Completed list table.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,40 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light mb-1">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" 
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#/">Games List</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#/addgame">Add Game</a>
-            </li>    
-        </ul>
+
+<nav class="navbar navbar-expand-lg bg-light mb-1">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" 
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <!-- <a class="nav-link active" aria-current="page" href="#/">Games List</a> -->
+                <router-link to="/" class="nav-link">Games List</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/addgame" class="nav-link">Add Game</router-link>
+                    <!-- <a class="nav-link" href="#/addgame">Add Game</a> -->
+                </li>    
+                <li class="nav-item">
+                    <router-link to="/statichtml" class="nav-link">Static HTML</router-link>
+                    <!-- <a class="nav-link" href="#/addgame">Add Game</a> -->
+                </li>    
+            </ul>
         </div>
+        <form class="form-inline d-none d-sm-none d-lg-inline">
+          <i class="fas fa-search" aria-hidden="true"></i>
+          <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search"
+            aria-label="Search">
+        </form>
     </div>
     </nav>
+
+    <div class="d-flex justify-content-center">
+        <!-- <component :is="currentView" /> -->
+        <router-view></router-view>
+    </div>
+
 </template>
---
+++ src/components/StaticHTML.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="d-flex justify-content-center">
+        <div class="mt-3 border d-flex justify-content-between flex-column" style="width: 723px; height: 531px; max-width: 80vw;">
+                <div>
+                    <table class="table">
+                        <thead>
+                        <tr class="right-border text-center">
+                            <th scope="col">Name <span>↑ ↓</span></th><!-- TODO add arrow icons -->
+                            <th scope="col">Publisher <span>↑ ↓</span></th><!-- TODO add arrow icons -->
+                            <th scope="col">Rating <span>↑ ↓</span></th><!-- TODO add arrow icons -->
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="text-center">
+                            <td>Game1</td>
+                            <td>Publisher1</td>
+                            <td>5</td>
+                            <td class="d-flex justify-content-center">
+                                <div class="d-flex w-50 gap-3">
+                                    <button  class='border-0 p-y-1 rounded' style="height: 30px; background-color: #F06C9B; " >Delete</button>
+                                    <button  class='border-0 p-y-1 rounded' style="height: 30px; background-color: #96C9DC; " >Edit</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="text-center">
+                            <td>Game2</td>
+                            <td>Publisher2</td>
+                            <td>5</td>
+                            <td class="d-flex justify-content-center">
+                                <div class="d-flex w-50 gap-3">
+                                    <button  class='border-0 p-y-1 rounded' style="height: 30px; background-color: #F06C9B; " >Delete</button>
+                                    <button  class='border-0 p-y-1 rounded' style="height: 30px; background-color: #96C9DC; " >Edit</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>{{ '<< < 12345678 > >> '}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.right-border th {
+    border-right: 1px solid black; 
+}
+</style>   

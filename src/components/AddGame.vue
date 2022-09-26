834c95b32fdc1629e40efdd5015189e087392705
rskidmore3 <rskidmore@ethika.com>
2022-09-26 13:43:25 -0700

    Added form.

--- src/components/AddGame.vue
+++ src/components/AddGame.vue
@@ -33,19 +33,24 @@
 }
 </script>
 <template>
-    <form @submit="submitForm" ref="anyName" >
+    <form @submit="submitForm" ref="anyName" class="mt-5"  style="width: 350px;">
         <div class="mb-3">
-            <label for="name" class="form-label">Name*</label>
-            <input type="text" class="form-control" id="nameOfGame" v-model="name" aria-describedby="nameOfGame">
+            <!-- <label for="name" class="form-label">Name*</label>
+            <input type="text" class="form-control" id="nameOfGame" v-model="name" aria-describedby="nameOfGame"> -->
+            <input type="text" class="form-control border-0 border-bottom" id="nameOfGame"  placeholder="Game" v-model="name"  aria-describedby="nameOfGame">
         </div>
         <div class="mb-3">
-            <label for="publisher" class="form-label">Publisher*</label>
-            <input type="text" class="form-control" id="publisher" v-model='publisher'>
+            <!-- <label for="publisher" class="form-label">Publisher*</label>
+            <input type="text" class="form-control" id="publisher" v-model='publisher'> -->
+            <input type="text" class="form-control border-0 border-bottom" id="publisher" placeholder="Publisher"  v-model='publisher'>
+
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Rating*</label>
-            <input type="number" class="form-control" id="rating" v-model="rating">
+            <!-- <label for="exampleInputPassword1" class="form-label">Rating*</label>
+            <input type="number" class="form-control" id="rating" v-model="rating"> -->
+            <input type="number" class="form-control border-0 border-bottom" id="rating"  placeholder="Rating" v-model="rating">
+
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="bg-transparent px-4" style="border-radius: 30px; border-width: 1px;">Submit</button>
     </form>
 </template>
